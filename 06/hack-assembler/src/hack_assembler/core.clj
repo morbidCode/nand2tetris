99cbb8b9ac22cbcc59c6b6310e909f3e3abfeb18
morbid code <morbidcode666@gmail.com>
2021-10-27 22:55:57 +0800

    added initial functions

--- 06/hack-assembler/src/hack_assembler/core.clj
+++ 06/hack-assembler/src/hack_assembler/core.clj
@@ -1,3 +1,16 @@
-
+(ns hack-assembler.core
+  (:require [clojure.string :as string]
+  [hack-assembler.io :as io]
+  [hack-assembler.parser :as parser :refer [parse]]
+  [hack-assembler.converter :as converter :refer [convert]]))
 
   (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+(let [{
+instructions :instructions
+labels :labels
+} (parse (io/load file-name))]
+(io/write (string/replace file-name ".asm" "/hack") (convert instructions labels))))
